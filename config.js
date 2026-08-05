c47b871b5b19eb8c2ef5bec3f5e16840c9cc1032
FunkyTimmy <rowan.p967@stu.nebo.edu>
2026-08-05 12:31:24 -0700

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 const SITE = {
   name: "GC Biology",
   tagline: "Study With the GC",
-  logo: "/images/Gc%20Biology%20Logo.png",
-  favicon: "/images/Gc%20Biology%20Logo.png", 
+  logo: "https://cdn.jsdelivr.net/gh/FunkyTimmy/GrouapChat-Biology@main/images/Gc%20Biology%20Logo.png",
+  favicon: "https://cdn.jsdelivr.net/gh/FunkyTimmy/GrouapChat-Biology@main/images/Gc%20Biology%20Logo.png", 
   titleSuffix: " - GC Biology"
 };
 
@@ -14,8 +14,8 @@ const GAMES = [
     name: "Cookie Clicker",
     description: "Click the cookie to get more cookies and the cycle repeats. Keep upgrading your cursors and farms to build an empire of baked goods.",
     genre: "Arcade",
-    thumbnail: "/images/cookieclicker.jpg",
-    fileUrl: "/games/cookieclicker.html",
+    thumbnail: "https://cdn.jsdelivr.net/gh/FunkyTimmy/little-timmy-codepen@main/EscapeRoad.png", // Placeholder if image missing
+    fileUrl: "games/cookieclicker.html",
     popular: false,
   },
   {
@@ -23,8 +23,8 @@ const GAMES = [
     name: "Minecraft 1.12.2",
     description: "Eaglercraft 1.12.2. As similar to Minecraft 1.12.2 as possible. Join xenaMC on multiplayer!",
     genre: "Arcade",
-    thumbnail: "/images/wp8691241-minecraft-thumbnail-wallpapers.jpg",
-    fileUrl: "/games/minecraft1-12-2.html",
+    thumbnail: "https://cdn.jsdelivr.net/gh/FunkyTimmy/little-timmy-codepen@main/EscapeRoad.png", // Placeholder
+    fileUrl: "games/minecraft1-12-2.html",
     popular: true,
   },
   {
@@ -33,7 +33,7 @@ const GAMES = [
     description: "You just robbed a bank and you have to run away from the police. Your wanted level gets higher every minute. How long can you survive?",
     genre: "Arcade",
     thumbnail: "https://cdn.jsdelivr.net/gh/FunkyTimmy/little-timmy-codepen@main/EscapeRoad.png",
-    fileUrl: "/games/escaperoad.html",
+    fileUrl: "games/escaperoad.html",
     popular: false,
   },
   {
@@ -41,8 +41,8 @@ const GAMES = [
     name: "Geometry Dash Lite",
     description: "Jump over obstacles while continuously moving to the beat.",
     genre: "Arcade",
-    thumbnail: "/images/gdlite.jpg",
-    fileUrl: "/games/gdlite.html",
+    thumbnail: "https://cdn.jsdelivr.net/gh/FunkyTimmy/little-timmy-codepen@main/EscapeRoad.png", // Placeholder
+    fileUrl: "games/gdlite.html",
     popular: false,
   }
 ];
@@ -53,6 +53,6 @@ const APPS = [
   //   name: "Desmos Graphing",
   //   description: "An online graphing calculator for math and science.",
   //   thumbnail: "https://cdn.jsdelivr.net/gh/FunkyTimmy/little-timmy-codepen@main/desmos.png",
-  //   fileUrl: "/apps/desmos.html"
+  //   fileUrl: "apps/desmos.html"
   // }
 ];
