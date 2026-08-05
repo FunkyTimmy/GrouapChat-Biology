5a302f7a19cb34783fa89201e1476bf26c287e3c
FunkyTimmy <rowan.p967@stu.nebo.edu>
2026-08-05 13:05:53 -0700

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 const SITE = {
   name: "GC Biology",
   tagline: "Study With the GC",
-  logo: "/images/Gc%20Biology%20Logo.png",
-  favicon: "/images/Gc%20Biology%20Logo.png", 
+  logo: "images/Gc Biology Logo.png",
+  favicon: "images/Gc Biology Logo.png", 
   titleSuffix: " - GC Biology"
 };
 
@@ -14,8 +14,8 @@ const GAMES = [
     name: "Cookie Clicker",
     description: "Click the cookie to get more cookies and the cycle repeats. Keep upgrading your cursors and farms to build an empire of baked goods.",
     genre: "Arcade",
-    thumbnail: "/images/cookieclicker.jpg/", 
-    fileUrl: "/games/cookieclicker.html",
+    thumbnail: "images/cookieclicker.jpg",
+    fileUrl: "games/cookieclicker.html",
     popular: false,
   },
   {
@@ -23,8 +23,8 @@ const GAMES = [
     name: "Minecraft 1.12.2",
     description: "Eaglercraft 1.12.2. As similar to Minecraft 1.12.2 as possible. Join xenaMC on multiplayer!",
     genre: "Arcade",
-    thumbnail: "/images/wp8691241-minecraft-thumbnail-wallpapers.jpg", 
-    fileUrl: "/games/minecraft1-12-2.html",
+    thumbnail: "images/wp8691241-minecraft-thumbnail-wallpapers.jpg",
+    fileUrl: "games/minecraft1-12-2.html",
     popular: true,
   },
   {
@@ -32,8 +32,8 @@ const GAMES = [
     name: "Escape Road",
     description: "You just robbed a bank and you have to run away from the police. Your wanted level gets higher every minute. How long can you survive?",
     genre: "Arcade",
-    thumbnail: "https://cdn.jsdelivr.net/gh/FunkyTimmy/little-timmy-codepen@main/EscapeRoad.png",
-    fileUrl: "/games/escaperoad.html",
+    thumbnail: "images/EscapeRoad.png",
+    fileUrl: "games/escaperoad.html",
     popular: false,
   },
   {
@@ -41,18 +41,18 @@ const GAMES = [
     name: "Geometry Dash Lite",
     description: "Jump over obstacles while continuously moving to the beat.",
     genre: "Arcade",
-    thumbnail: "/images/gdlite.jpg/", 
-    fileUrl: "/games/gdlite.html",
+    thumbnail: "images/gdlite.jpg",
+    fileUrl: "games/gdlite.html",
     popular: false,
   }
 ];
 
 const APPS = [
-  // {
-  //   id: "desmos",
-  //   name: "Desmos Graphing",
-  //   description: "An online graphing calculator for math and science.",
-  //   thumbnail: "https://cdn.jsdelivr.net/gh/FunkyTimmy/little-timmy-codepen@main/desmos.png",
-  //   fileUrl: "apps/desmos.html"
-  // }
+  {
+    id: "desmos",
+    name: "Desmos Graphing",
+    description: "An online graphing calculator for math and science.",
+    thumbnail: "images/desmos.png",
+    fileUrl: "apps/desmos.html"
+  }
 ];
